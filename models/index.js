6d6ea5a4fc9ee83875cfe60097679e6f3e533780
jbui <jeremyabui@yahoo.com>
2019-12-03 12:16:05 -0800

    Created base MVC

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const dbUrl = process.env.MONGODB_URI || 'Connected to server';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(`MongoDB connection error: ${err}`));
+
+module.exports = {
+
+}
